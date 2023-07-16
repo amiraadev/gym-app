6e3f9e77acc6dbee65a7e8e367c3879aa96266b2
amirticha <allagui_fawzi@outlook.fr>
2023-07-16 21:11:02 +0100

    commit 3

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from '@/assets/SponsorRedBull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage : (value:SelectedPage) => void;
@@ -19,19 +20,40 @@ function Home({setSelectedPage}: Props) {
             id="home"
             className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
      >
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <div><img src={HomePageText} alt="home page text" /></div>
+        <div  className=' md:flex mx-auto w-5/6 items-center justify-center md:h-5/6' >
+            <div  className='z-10  mt-32 md:basis-3/5 '>
+                <motion.div 
+                     className='md:-mt-20'
+                     initial="hidden"
+                     whileInView='visible'
+                     viewport={{once:true,amount:0.5}}
+                     transition={{duration:0.5}}
+                     variants={{
+                        hidden:{opacity:0,x: -50},
+                        visible:{opacity:1,x: -0},
+                     }}
+                     >
+                    <div  className='relative'>
+                        <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'><img src={HomePageText} alt="home page text" /></div>
                     </div>
-                    <p>
+                    <p className='mt-8 text-sm '>
                     Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
                     </p>
-                </div>
-                <div>
+                </motion.div>
+
+
+                <motion.div 
+                className='mt-8 flex items-center gap-8 '
+                initial="hidden"
+                whileInView='visible'
+                viewport={{once:true,amount:0.5}}
+                transition={{delay:0.2,duration:0.5}}
+                variants={{
+                   hidden:{opacity:0,x: -50},
+                   visible:{opacity:1,x: -0},}}
+                >
                     <ActionButton setSelectedPage={setSelectedPage} >
                         Join now 
                     </ActionButton>
@@ -41,19 +63,20 @@ function Home({setSelectedPage}: Props) {
                       href={`#${SelectedPage.ContactUs}`}>
                         <p>Learn More</p>
                       </AnchorLink>
-                </div>
+                </motion.div>
             </div>
 
 
-            <div>
+            <div className='flex basis-3/5 justify-center md:z-10
+            md:ml-40 md:mt-16 md:justify-items-ent'>
               <img src={HomePageGraphic} alt="home page graphic" />
             </div>
         </div>
 
         {isAboveMediumScreens && (
-            <div>
-                <div>
-                    <div>
+            <div className='h-[150px] w-full bg-primaty-100 py-10'>
+                <div className='mx-auto w-5/6 '>
+                    <div className='flex w-3/5 items-center justify-between gap-8'>
                         <img src={SponsorRedBull} alt="red bull sponsor" />
                         <img src={SponsorForbes} alt="forbes sponsor" />
                         <img src={SponsorFortune} alt="fortune sponsor" />
