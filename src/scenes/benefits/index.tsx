02c8f2bf4316c9f4b42b52c61aef93a6eca1e69c
amirticha <allagui_fawzi@outlook.fr>
2023-07-16 22:47:53 +0100

    commit 6

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -7,7 +7,23 @@ import { motion } from 'framer-motion';
 type Props = {
     setSelectedPage :(value:SelectedPage) =>void
 }
-
+const benefits =[
+    {
+        icon :<HomeModernIcon className="h-6 w-6"/>,
+        title:"State of the art facilities",
+        description:   "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
+    },
+    {
+        icon :<UserGroupIcon className="h-6 w-6"/>,
+        title:"100's of Diverse Classes",
+        description:   "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
+    },
+    {
+        icon :<AcademicCapIcon className="h-6 w-6"/>,
+        title:"Expert and Pro trainer",
+        description:   "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
+    },
+]
 export default function Benefits({setSelectedPage}: Props) {
   return (
     <section 
@@ -16,12 +32,21 @@ export default function Benefits({setSelectedPage}: Props) {
         <motion.div
            onViewportEnter={() => setSelectedPage(SelectedPage.Benefits) }
           >
-            <div>
-              <Htext>MORE THAN JUST A GYM </Htext>   
-              <p> We provide world class fitness equipment, trainers and classes to
-            get you to your ultimate fitness goals with ease. We provide true
-            care into each and every member.</p>       
+            {/* HEADER */}
+            <div className="md:w-3/5 md:my-5 ">
+                <Htext>MORE THAN JUST A GYM </Htext>   
+                    <p className="my-5 text-sm "> We provide world class fitness equipment, trainers and classes to
+                    get you to your ultimate fitness goals with ease. We provide true
+                    care into each and every member.</p>       
              </div>
+
+             {/* bENEFITS */}
+             <div className="md:flex items-center justify-betwwen gap-8 mt-5 ">
+                {Benefits.map((Benefit) =>(
+                    <Benefit/>
+                ))}
+             </div>
+
         </motion.div>
     
     </section >
