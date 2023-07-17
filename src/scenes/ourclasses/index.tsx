af33d316f456ba4edfcb06e75e711e21993f7ebe
amirticha <allagui_fawzi@outlook.fr>
2023-07-17 12:23:59 +0100

    7th commit

--- src/scenes/ourclasses/index.tsx
+++ src/scenes/ourclasses/index.tsx
@@ -1,4 +1,4 @@
-import { SelectedPage } from '@/shared/types'
+import { ClassType, SelectedPage } from '@/shared/types'
 import image1 from "@/assets/image1.png"
 import image2 from "@/assets/image1.png"
 import image3 from "@/assets/image1.png"
@@ -6,15 +6,89 @@ import image4 from "@/assets/image1.png"
 import image5 from "@/assets/image1.png"
 import image6 from "@/assets/image1.png"
 import { motion } from 'framer-motion';
+import Htext from '@/shared/Htext'
+import Class from './Class'
 
 type Props = {
     setSelectedPage :(value:SelectedPage) =>void
 }
 
+const classes: Array<ClassType> = [
+    {
+      name: "Weight Training Classes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      image: image1,
+    },
+    {
+      name: "Yoga Classes",
+      image: image2,
+    },
+    {
+      name: "Ab Core Classes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      image: image3,
+    },
+    {
+      name: "Adventure Classes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      image: image4,
+    },
+    {
+      name: "Fitness Classes",
+      image: image5,
+    },
+    {
+      name: "Training Classes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      image: image6,
+    },
+  ];
+
 export default function OurClasses({setSelectedPage}: Props) {
   return (
     <section id="ourclasses" className='w-full bg-primary-100 py-40'>
-        <motion.div>
+        <motion.div 
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+        >
+            <motion.div
+             className='mx-auto w-5/6 '
+             initial="hidden"
+             whileInView='visible'
+             viewport={{once:true,amount:0.5}}
+             transition={{duration:0.5}}
+             variants={{
+                hidden:{opacity:0,x: -50},
+                visible:{opacity:1,x: -0},
+             }}
+             >
+                <div>
+                    <Htext>
+                        Our Classes
+                    </Htext>
+                    <p className="py-5">
+                        Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                        tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                        facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                        enim mattis odio in risus nunc.
+                    </p>
+                </div>
+            </motion.div>
+            <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
 
         </motion.div>
     </section>
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -11,3 +11,8 @@ export enum SelectedPage {
     title:string;
     description:string;
   }
+  export interface ClassType {
+    name: string;
+    description?: string;
+    image: string;
+  }
