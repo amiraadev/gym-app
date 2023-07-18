ddcaea11d628b55f3a83dc0e555ecf55755ed4cd
amirticha <allagui_fawzi@outlook.fr>
2023-07-18 22:04:19 +0100

    12

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,17 +13,6 @@ export default {
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
       },
-      // colors: {
-      //   "gray-20": "#F8F4EB",
-      //   "gray-50": "#EFE6E6",
-      //   "gray-100": "#DFCCCC",
-      //   "gray-500": "#5E0000",
-      //   "primary-100": "#FFE1E0",
-      //   "primary-300": "#FFA6A3",
-      //   "primary-500": "#FF6B66",
-      //   "secondary-400": "#FFCD5B",
-      //   "secondary-500": "#FFC132",
-      // },
       backgroundImage: (theme) => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
