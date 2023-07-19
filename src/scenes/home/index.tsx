dfaa50e8ee4b891d07670f5089beb9e2ac8d11c2
amirticha <allagui_fawzi@outlook.fr>
2023-07-19 11:02:23 +0100

    animation commit

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -75,7 +75,7 @@ function Home({setSelectedPage}: Props) {
                 viewport={{once:true,amount:0.5}}
                 transition={{delay:0.2,duration:0.5}}
                 variants={{
-                   hidden:{opacity:0,x: 150},
+                   hidden:{opacity:0,x: 130},
                    visible:{opacity:1,x: 0},}}
                    >
                 <img src={HomePageGraphic} alt="home page graphic" />
